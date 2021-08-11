72045282469d912aa839738f8b278fde27c79c60
r0m3c <leandrogamarra@rocketmail.com>
2021-08-11 19:34:58 -0400

    Initial Commit

---
+++ Movie Library/Movie_LibraryApp.swift
@@ -0,0 +1,20 @@
+//
+//  Movie_LibraryApp.swift
+//  Movie Library
+//
+//  Created by Leandro Gamarra on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Movie_LibraryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
